a54fde0648a4c0996a2ea098840ce1be14916a75
EzequielCaste <ezequiel.caste@gmail.com>
2021-05-04 21:56:12 -0300

    fix styles for mobile display

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const Hero = () => {
   const btnClass =
-    'px-3 mr-1 text-2xl hover:text-gray-200 rounded bg-green-500 cursor-pointer text-white p-2';
+    'px-8 mr-1 text-2xl hover:text-gray-200 rounded bg-green-500 cursor-pointer text-white p-2';
   return (
     <section id="main" className="shadow-lg">
       <div className="overlay">
-        <div className="sm:text-2xl inner p-2.5 sm:p-16">
+        <div className="sm:text-2xl space-y-4 inner p-2.5 sm:p-16">
           <h1 className="font-bold text-4xl sm:text-7xl">AO Copiers</h1>
           <h2 className="mt-4">Alquiler y Venta de Equipos de Impresion</h2>
-          <h2 className="sm:text-lg">
+          {/* <h2 className="sm:text-lg">
             <i className="fab fa-whatsapp"></i> +54 9 3874 56-5222
           </h2>
           <h2 className="sm:text-lg">
             <i className="far fa-envelope"></i> ao_copiers@yahoo.com.ar
-          </h2>
-          <div className="mt-5">
+          </h2> */}
+          <div className="my-5 mx-auto sm:mx-0 sm:items-center sm:justify-center">
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -33,6 +33,12 @@ const Hero = () => {
               <i className="far fa-envelope"></i>
             </a>
           </div>
+          <h2 className="sm:text-lg">
+            <i className="fab fa-whatsapp"></i> +54 9 3874 56-5222
+          </h2>
+          <h2 className="sm:text-lg">
+            <i className="far fa-envelope"></i> ao_copiers@yahoo.com.ar
+          </h2>
         </div>
       </div>
     </section>
